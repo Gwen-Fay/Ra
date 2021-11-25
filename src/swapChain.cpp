d6f17845ec41a356cf805bc98f8802ce49b3fa52
Gwen Benzschawel <coffeeplusplus17@gmail.com>
2021-11-25 13:22:34 -0600

    set color correction to SRGB

--- src/swapChain.cpp
+++ src/swapChain.cpp
@@ -365,7 +365,7 @@ void SwapChain::createSyncObjects() {
 VkSurfaceFormatKHR SwapChain::chooseSwapSurfaceFormat(
     const std::vector<VkSurfaceFormatKHR> &availableFormats) {
   for (const auto &availableFormat : availableFormats) {
-    if (availableFormat.format == VK_FORMAT_B8G8R8A8_UNORM &&
+    if (availableFormat.format == VK_FORMAT_B8G8R8A8_SRGB &&
         availableFormat.colorSpace == VK_COLOR_SPACE_SRGB_NONLINEAR_KHR) {
       return availableFormat;
     }
