c16848e1a93de0ef448939ef311b227a05797159
Gwen Benzschawel <coffeeplusplus17@gmail.com>
2021-12-29 16:58:53 -0600

    testing now works again

--- debugBuild.sh
+++ debugBuild.sh
@@ -6,7 +6,8 @@ echo "-----------------------"
 cmake -S . -B build -D CMAKE_CXX_COMPILER=clang++\
     -D CMAKE_BUILD_TYPE=Debug -D PLATFORM="LINUX" || exit 1
 make -C build || exit 1
-cd build/bin
-ctest || exit 1
+cd build/libgaem
+ctest --test-dir ../bin  || exit 1
+cd ../bin
 lldb Demo -o r
 exit 0
